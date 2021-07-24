f387ebf9ad2f603b7a2b03b3d4abf649baac0a59
isaachew <isaachewks@gmail.com>
2021-07-24 17:21:27 +0800

    Added subpath selection

--- patheditor.js
+++ patheditor.js
@@ -49,9 +49,8 @@ function tocc(path){
 }
 console.log(0)
 */
-let cpath=pto("M-250 0L-200 0")
-cpath=pto("M3.472-.845C2.9828-.2265 2.0329-.2496 1.5695-1.0451S1.1317-3.0026 1.5398-3.751 3.2614-4.6612 3.472-3.149V-.845M4.9692-.586C4.3047-.3092 4.304-.8216 4.291-1.0477L4.2769-6.9428H4.0571L2.7215-6.4 2.7861-6.228C3.5168-6.5171 3.4313-5.9436 3.4598-5.7443V-4.2301C2.3034-5.2621.6854-4.0487.4284-2.7996.2831-2.0346.186-.2199 1.8705.1145 2.6486.2517 3.1213-.1226 3.472-.4935L3.4711.1322H3.6974L5.0244-.4157Z")
-let curpath
+let curpath=pto("M-250 0L-200 0")
+curpath=pto("M3.472-.845C2.9828-.2265 2.0329-.2496 1.5695-1.0451S1.1317-3.0026 1.5398-3.751 3.2614-4.6612 3.472-3.149V-.845M4.9692-.586C4.3047-.3092 4.304-.8216 4.291-1.0477L4.2769-6.9428H4.0571L2.7215-6.4 2.7861-6.228C3.5168-6.5171 3.4313-5.9436 3.4598-5.7443V-4.2301C2.3034-5.2621.6854-4.0487.4284-2.7996.2831-2.0346.186-.2199 1.8705.1145 2.6486.2517 3.1213-.1226 3.472-.4935L3.4711.1322H3.6974L5.0244-.4157ZM6 0V-6.8L6.8-7V-3.8C7.6-4.6 9.4-4.4 9.4-2.6V0H8.6V-2.8C8.6-3.4 7.2-3.6 6.8-2.8V0Z")
 //Smooth Bezier curve has a controls point opposite of the previous
 //Paths are delimited by moveTo or closePath or both (moveTo starts a subpath)
 function draw(){
@@ -70,7 +69,7 @@ function draw(){
     //*/
     context.strokeStyle="black"
     context.fillStyle="lime"
-    pth=new Path2D(otp(cpath))
+    pth=new Path2D(otp(curpath))
     context.lineWidth=1.5/view.scale
     if(fill)context.fill(pth,fillrule?"evenodd":"nonzero")
     context.stroke(pth)
@@ -79,7 +78,7 @@ function draw(){
     context.strokeStyle="#bbb"
     context.lineWidth=3/view.scale
     function drawpoint(x,y){context.fillRect(x-2.5/view.scale,y-2.5/view.scale,5/view.scale,5/view.scale)}
-    for(i of cpath){
+    for(let i of curpath){
         let lp=i.start
         drawpoint(...i.start)
         for(j of i){
@@ -113,10 +112,41 @@ function draw(){
         
     }
     context.fillStyle="#0f0"
-    if(selected)drawpoint(...cpath[selected.subpath].start)
+    if(selected)drawpoint(...curpath[selected.subpath].start)
     context.restore()
-    document.getElementById("pathtxt").value=otp(cpath)
+    document.getElementById("pathtxt").value=otp(curpath)
     document.getElementById("scale").textContent=view.width/view.scale
+    
+    
+    
+}
+function subpaths(){
+	document.getElementById("subpaths").innerHTML=""
+    for(var i=0;i<curpath.length;i++){    
+		var dv=document.createElement("div")
+		dv.className="subpathlist"
+		dv.id="subp"+i
+		dv.addEventListener("click",function(){
+			selected={subpath:+this.id.slice(4)}
+			cfsels()
+			draw()
+		})
+		var spn=document.createElement("span")
+		spn.append("Subpath "+-~i)
+		dv.append(spn)
+		document.getElementById("subpaths").append(dv)
+    }
+}
+function cfsels(){
+	document.getElementById("commands").textContent=JSON.stringify(selected)
+	
+	subpaths()
+	document.getElementById("subp"+selected.subpath).className="selected"
+	
+	
+	
+	
+	
 }
 rtn=(n,r)=>r?+(Math.round(n/r)*r).toFixed(12):n
 let subpath,selected={subpath:0},mousestate=false
@@ -127,14 +157,14 @@ canv.addEventListener("mousedown",e=>{
     [mcx,mcy]=getmxy(e)
     mousestate=1
     
-    let subpath=cpath[selected.subpath]
+    let subpath=curpath[selected.subpath]
     var lcmd=subpath[subpath.length-1]
     var lc=subpath.length?lcmd.to:subpath.start
     var lastControl=subpath.length?lcmd.type=="bezier"?lcmd.controls[1]:lcmd.type=="quadratic"?lcmd.control:lc:lc
     nearpoint=(a,b=[mcx,mcy])=>Math.hypot(a[0]-b[0],a[1]-b[1])<20/view.scale
-    for(i=0;i<cpath.length;i++){
-        for(j=0;j<cpath[i].length;j++){
-            let pcmd=cpath[i][j]
+    for(i=0;i<curpath.length;i++){
+        for(j=0;j<curpath[i].length;j++){
+            let pcmd=curpath[i][j]
             var sels
             switch(pcmd.type){
                 case "bezier":sels={c1:pcmd.controls[0],c2:pcmd.controls[1]}
@@ -148,10 +178,11 @@ canv.addEventListener("mousedown",e=>{
             }
             if(nearpoint(pcmd.to))selected={subpath:i,command:j,selection:"to"}
         }
-        if(nearpoint(cpath[i].start))selected={subpath:i,selection:"start"}
+        if(nearpoint(curpath[i].start))selected={subpath:i,selection:"start"}
     }
     if(selected.selection)ctool=null
     draw()
+    cfsels()
 })
 canv.addEventListener("mousemove",e=>{
     [mcx,mcy]=getmxy(e)
@@ -160,7 +191,7 @@ canv.addEventListener("mousemove",e=>{
     if(mousestate==1){
         if(selected.selection){
             
-            let subpath=cpath[selected.subpath]
+            let subpath=curpath[selected.subpath]
             if(selected.selection=="start")subpath.start=[mcx,mcy]
             else{
                 var lcmd=subpath[selected.command]
@@ -188,7 +219,7 @@ canv.addEventListener("mousemove",e=>{
 canv.addEventListener("mouseup",e=>{
     [mcx,mcy]=getmxy(e)
     
-    let subpath=cpath[selected.subpath]
+    let subpath=curpath[selected.subpath]
     var lcmd=subpath[subpath.length-1]
     var lastPoint=subpath.length?lcmd.to:subpath.start
     var lastControl=subpath.length?lcmd.type=="bezier"?lcmd.controls[1]:lcmd.type=="quadratic"?lcmd.control:lastPoint:lastPoint
@@ -234,22 +265,22 @@ addEventListener("resize",e=>{
 })
 
 document.getElementById("canvwrapper").addEventListener("keydown",e=>{
-    if(e.key=="Delete")cpath[selected.subpath].pop()
+    if(e.key=="Delete")curpath[selected.subpath].pop()
     draw()
 },{passive:true})
 
 document.getElementById("pathtxt").addEventListener("input",e=>{
-    cpath=pto(e.target.value)
+    curpath=pto(e.target.value)
     selected={subpath:0}
     draw()
-    
+    cfsels()
     
     
     
 })
 
 document.getElementById("pclosed").addEventListener("input",e=>{
-	cpath[selected.subpath].closed=e.target.checked
+	curpath[selected.subpath].closed=e.target.checked
 	draw()
 })
 
